153c23ff0645952eb1536dc2c47cdc0010f802c0
unknown <hrosso.martin@gmail.com>
2019-02-03 17:30:52 +0100

    3

--- run_scripts_pa.py
+++ run_scripts_pa.py
@@ -8,30 +8,30 @@ from config import config
 # Import list of traded pairs form get_timeframe_data file
 from inputs import ALLpairs
 
-    params = config()
-    conn = psycopg2.connect(**params)
-    cur = conn.cursor()
+params = config()
+conn = psycopg2.connect(**params)
+cur = conn.cursor()
 
-    for pair in ALLpairs:
-        last1m = list(get_price_data(pair, '1m', hist=False))
-        holder = int(str(last1m[0])[:-3])
-        ts = time.gmtime(holder)
-        last1m[0] = time.strftime("%Y-%m-%d %H:%M", ts)
-        print(last1m)
-        print(pair[0])
-        cur.execute("DROP TABLE IF EXISTS data_1m_" + str(pair[0]) + "_" + str(pair[1]))
-        cur.execute("CREATE TABLE IF NOT EXISTS data_1m_" + str(pair[0]) + "_" + str(pair[1]) + """(
-                        timestamp timestamp PRIMARY KEY,
-                        open DECIMAL(13,8),
-                        close DECIMAL(13,8),
-                        high DECIMAL(13,8),
-                        low DECIMAL(13,8),
-                        volume DECIMAL(13,8));""")
-        cur.execute("INSERT INTO data_1m_" +
-                        str(pair[0]) + "_" + str(pair[1]) +
-                        " VALUES(%s, %s, %s, %s, %s, %s)", last1m)
-        conn.commit()
-    conn.close()
+for pair in ALLpairs:
+    last1m = list(get_price_data(pair, '1m', hist=False))
+    holder = int(str(last1m[0])[:-3])
+    ts = time.gmtime(holder)
+    last1m[0] = time.strftime("%Y-%m-%d %H:%M", ts)
+    print(last1m)
+    print(pair[0])
+    cur.execute("DROP TABLE IF EXISTS data_1m_" + str(pair[0]) + "_" + str(pair[1]))
+    cur.execute("CREATE TABLE IF NOT EXISTS data_1m_" + str(pair[0]) + "_" + str(pair[1]) + """(
+                    timestamp timestamp PRIMARY KEY,
+                    open DECIMAL(13,8),
+                    close DECIMAL(13,8),
+                    high DECIMAL(13,8),
+                    low DECIMAL(13,8),
+                    volume DECIMAL(13,8));""")
+    cur.execute("INSERT INTO data_1m_" +
+                    str(pair[0]) + "_" + str(pair[1]) +
+                    " VALUES(%s, %s, %s, %s, %s, %s)", last1m)
+    conn.commit()
+conn.close()
 
 ### Save to 1m database
 
